jbyuki/one-small-step-for-vimkind

commit 319346e4d977623c7af66c1c520531b68b8c3542

launch the headless instance in a clean environment (#64)

lua/osv/init.lua
@@ -107,17 +107,6 @@ function M.launch(opts)
     if opts.output ~= nil then redir_nvim_output = opts.output end
   end
 
-  if not opts or not opts.recursive then
-    local env = {}
-    for k,v in pairs(vim.fn.environ()) do
-    	env[k] = v
-    end
-
-    if env["HEADLESS_OSV"] then
-      return
-    end
-  end
-
   if M.is_running() then
   	vim.api.nvim_echo({{"Server is already running.", "ErrorMsg"}}, true, {})
     return
@@ -129,62 +118,17 @@ function M.launch(opts)
     log_filename = vim.fn.stdpath("data") .. "/osv.log"
   end
 
-  local has_embed = false
-  local has_headless = false
-  local args = {}
-  local i = 1
-  while i <= #vim.v.argv do 
-    local skiparg = false
-    local arg = vim.v.argv[i]
-  	if arg == '--embed' then
-  		has_embed = true
-  	elseif arg == '--headless' then
-  		has_headless = true
-    elseif arg == '--listen' then
-      skiparg = true
-      i = i + 1
-  	end
-    if not skiparg then
-      table.insert(args, arg)
-    end
-    i = i + 1
-  end
-
-  if not has_embed then
-  	table.insert(args, "--embed")
-  end
-
-  if not has_headless then
-  	table.insert(args, "--headless")
-  end
-
-  local env = {}
-  for k,v in pairs(vim.fn.environ()) do
-  	env[k] = v
-  end
-
-  if opts and opts.env then
-  	env = env or {}
-  	for k,v in pairs(opts.env) do
-  		env[k] = v
-  	end
-  end
-
-  if opts and opts.args then
-  	for _, arg in ipairs(opts.args) do
-  		table.insert(args, arg)
-  	end
-  end
-
-  env["HEADLESS_OSV"] = true
-
   if M.on["start_server"] then
     nvim_server = M.on["start_server"](args, env)
     assert(nvim_server)
 
 
   else
-    nvim_server = vim.fn.jobstart(args, {rpc = true, env = env})
+    local clean_args = { vim.v.progpath, '-u', 'NONE', '-i', 'NONE', '-n', '--embed', '--headless' }
+    nvim_server = vim.fn.jobstart(clean_args, {rpc = true})
+    vim.fn.rpcrequest(nvim_server, 'nvim_exec_lua', 'vim.o.runtimepath = ...', { vim.o.runtimepath })
+    auto_nvim = nvim_server
+
   end
 
   local mode = vim.fn.rpcrequest(nvim_server, "nvim_get_mode")
@@ -1600,42 +1544,6 @@ function M.run_this(opts)
     vim.fn.jobstop(auto_nvim)
     auto_nvim = nil
   end
-
-  local has_embed = false
-  local has_headless = false
-  local args = {}
-  local i = 1
-  while i <= #vim.v.argv do 
-    local skiparg = false
-    local arg = vim.v.argv[i]
-  	if arg == '--embed' then
-  		has_embed = true
-  	elseif arg == '--headless' then
-  		has_headless = true
-    elseif arg == '--listen' then
-      skiparg = true
-      i = i + 1
-  	end
-    if not skiparg then
-      table.insert(args, arg)
-    end
-    i = i + 1
-  end
-
-  if not has_embed then
-  	table.insert(args, "--embed")
-  end
-
-  if not has_headless then
-  	table.insert(args, "--headless")
-  end
-
-  local env = {}
-  for k,v in pairs(vim.fn.environ()) do
-  	env[k] = v
-  end
-
-  auto_nvim = vim.fn.jobstart(args, {rpc = true, env = env})
 
   assert(auto_nvim, "Could not create neovim instance with jobstart!")
 

src/run_this.lua.t
@@ -13,9 +13,8 @@ function M.run_this(opts)
 end
 
 @create_neovim_instance+=
-@copy_args
-@copy_env
-auto_nvim = vim.fn.jobstart(args, {rpc = true, env = env})
+@launch_headless_instance_with_clean_environment+=
+auto_nvim = nvim_server
 
 @launch_osv_server+=
 local server = vim.fn.rpcrequest(auto_nvim, "nvim_exec_lua", [[return require"osv".launch(...)]], { opts })

src/launch.lua.t
@@ -2,7 +2,6 @@
 @implement+=
 function M.launch(opts)
   @verify_launch_arguments
-  @check_recursive_if_not_disabled
   @abort_early_if_already_running
 
   @init_logger
@@ -27,14 +26,9 @@ end
 local nvim_server
 
 @spawn_nvim_instance_for_server+=
-@copy_args
-@copy_env
-@fill_env_with_custom
-@fill_args_with_custom
-@set_env_for_headless_instance
 @if_exists_use_custom_for_launching_server
 else
-  nvim_server = vim.fn.jobstart(args, {rpc = true, env = env})
+  @launch_headless_instance_with_clean_environment
 end
 
 @script_variables+=
@@ -99,55 +93,6 @@ if mode.blocking then
 	return
 end
 
-@fill_env_with_custom+=
-if opts and opts.env then
-	env = env or {}
-	for k,v in pairs(opts.env) do
-		env[k] = v
-	end
-end
-
-@fill_args_with_custom+=
-if opts and opts.args then
-	for _, arg in ipairs(opts.args) do
-		table.insert(args, arg)
-	end
-end
-
-@copy_args+=
-local has_embed = false
-local has_headless = false
-local args = {}
-local i = 1
-while i <= #vim.v.argv do 
-  local skiparg = false
-  local arg = vim.v.argv[i]
-	if arg == '--embed' then
-		has_embed = true
-	elseif arg == '--headless' then
-		has_headless = true
-  @skip_listen_arg
-	end
-  if not skiparg then
-    table.insert(args, arg)
-  end
-  i = i + 1
-end
-
-if not has_embed then
-	table.insert(args, "--embed")
-end
-
-if not has_headless then
-	table.insert(args, "--headless")
-end
-
-@copy_env+=
-local env = {}
-for k,v in pairs(vim.fn.environ()) do
-	env[k] = v
-end
-
 @abort_early_if_already_running+=
 if M.is_running() then
 	vim.api.nvim_echo({{"Server is already running.", "ErrorMsg"}}, true, {})
@@ -164,17 +109,6 @@ if M.on["start_server"] then
   assert(nvim_server)
 
 
-@check_recursive_if_not_disabled+=
-if not opts or not opts.recursive then
-  @copy_env
-  if env["HEADLESS_OSV"] then
-    return
-  end
-end
-
-@set_env_for_headless_instance+=
-env["HEADLESS_OSV"] = true
-
 @wait_attach_blocking+=
 while true do
   @wait_for_attach_message
@@ -184,7 +118,3 @@ end
 
 M.attach()
 
-@skip_listen_arg+=
-elseif arg == '--listen' then
-  skiparg = true
-  i = i + 1

src/launch_headless.lua.t
@@ -1,0 +1,5 @@
+##lua-debug
+@launch_headless_instance_with_clean_environment+=
+local clean_args = { vim.v.progpath, '-u', 'NONE', '-i', 'NONE', '-n', '--embed', '--headless' }
+nvim_server = vim.fn.jobstart(clean_args, {rpc = true})
+vim.fn.rpcrequest(nvim_server, 'nvim_exec_lua', 'vim.o.runtimepath = ...', { vim.o.runtimepath })

commit 17d4ff0f54da98e27f89d5578f357f99b5eed204

fix: fix Could not create neovim instance with jobstart! (#67)
Fix a regression introduced in 319346e4d977623c7af66c1c520531b68b8c3542

lua/osv/init.lua
@@ -127,8 +127,6 @@ function M.launch(opts)
     local clean_args = { vim.v.progpath, '-u', 'NONE', '-i', 'NONE', '-n', '--embed', '--headless' }
     nvim_server = vim.fn.jobstart(clean_args, {rpc = true})
     vim.fn.rpcrequest(nvim_server, 'nvim_exec_lua', 'vim.o.runtimepath = ...', { vim.o.runtimepath })
-    auto_nvim = nvim_server
-
   end
 
   local mode = vim.fn.rpcrequest(nvim_server, "nvim_get_mode")
@@ -1544,6 +1542,11 @@ function M.run_this(opts)
     vim.fn.jobstop(auto_nvim)
     auto_nvim = nil
   end
+
+  local clean_args = { vim.v.progpath, '-u', 'NONE', '-i', 'NONE', '-n', '--embed', '--headless' }
+  nvim_server = vim.fn.jobstart(clean_args, {rpc = true})
+  vim.fn.rpcrequest(nvim_server, 'nvim_exec_lua', 'vim.o.runtimepath = ...', { vim.o.runtimepath })
+  auto_nvim = nvim_server
 
   assert(auto_nvim, "Could not create neovim instance with jobstart!")
 

src/run_this.lua.t
@@ -13,7 +13,7 @@ function M.run_this(opts)
 end
 
 @create_neovim_instance+=
-@launch_headless_instance_with_clean_environment+=
+@launch_headless_instance_with_clean_environment
 auto_nvim = nvim_server
 
 @launch_osv_server+=