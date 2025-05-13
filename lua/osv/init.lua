9e4c9307cc5cec51015bdb3b4916d508801479a9
Julien Burkhard <jyburkhard@hotmail.com>
2025-05-13 10:48:34 +0900

    remove output functions in freeze loop

--- lua/osv/init.lua
+++ lua/osv/init.lua
@@ -1282,31 +1282,6 @@ function M.attach()
 
       				  M.server_messages = {}
 
-      				  if redir_nvim_output and not vim.in_fast_event() then
-      				    start_profiler("output")
-
-      				    -- Sets `g:_osv_nvim_output` with messages observed since last `:redir`.
-      				    pcall(vim.api.nvim_exec2, 'silent redir END', nvim_exec2_opts)
-
-      				    local ok, msgs = pcall(vim.api.nvim_get_var, '_osv_nvim_output')
-      				    if ok then
-      				      if type(msgs) ~= 'string' then
-      				        error('expected g:_osv_nvim_output to be a string but got: ' .. msgs)
-      				      elseif #msgs > 0 then
-      				        local event = make_event 'output'
-      				        event.body = { category = 'stdout', output = msgs }
-      				        sendProxyDAP(event)
-      				      end
-      				    end
-
-      				    -- Sets `g:_osv_nvim_output` as redir target for nvim messages, creates the
-      				    -- variable if it doesn't already exist, and clears it. Messages are still
-      				    -- displayed on-screen in debuggee as usual. This should also work when nvim
-      				    -- is headless.
-      				    pcall(vim.api.nvim_exec2, 'silent redir => g:_osv_nvim_output', nvim_exec2_opts)
-      				    stop_profiler("output")
-      				  end
-
       				  vim.wait(50)
       				end
 
@@ -1381,31 +1356,6 @@ function M.attach()
 
     		  M.server_messages = {}
 
-    		  if redir_nvim_output and not vim.in_fast_event() then
-    		    start_profiler("output")
-
-    		    -- Sets `g:_osv_nvim_output` with messages observed since last `:redir`.
-    		    pcall(vim.api.nvim_exec2, 'silent redir END', nvim_exec2_opts)
-
-    		    local ok, msgs = pcall(vim.api.nvim_get_var, '_osv_nvim_output')
-    		    if ok then
-    		      if type(msgs) ~= 'string' then
-    		        error('expected g:_osv_nvim_output to be a string but got: ' .. msgs)
-    		      elseif #msgs > 0 then
-    		        local event = make_event 'output'
-    		        event.body = { category = 'stdout', output = msgs }
-    		        sendProxyDAP(event)
-    		      end
-    		    end
-
-    		    -- Sets `g:_osv_nvim_output` as redir target for nvim messages, creates the
-    		    -- variable if it doesn't already exist, and clears it. Messages are still
-    		    -- displayed on-screen in debuggee as usual. This should also work when nvim
-    		    -- is headless.
-    		    pcall(vim.api.nvim_exec2, 'silent redir => g:_osv_nvim_output', nvim_exec2_opts)
-    		    stop_profiler("output")
-    		  end
-
     		  vim.wait(50)
     		end
 
@@ -1444,31 +1394,6 @@ function M.attach()
 
         M.server_messages = {}
 
-        if redir_nvim_output and not vim.in_fast_event() then
-          start_profiler("output")
-
-          -- Sets `g:_osv_nvim_output` with messages observed since last `:redir`.
-          pcall(vim.api.nvim_exec2, 'silent redir END', nvim_exec2_opts)
-
-          local ok, msgs = pcall(vim.api.nvim_get_var, '_osv_nvim_output')
-          if ok then
-            if type(msgs) ~= 'string' then
-              error('expected g:_osv_nvim_output to be a string but got: ' .. msgs)
-            elseif #msgs > 0 then
-              local event = make_event 'output'
-              event.body = { category = 'stdout', output = msgs }
-              sendProxyDAP(event)
-            end
-          end
-
-          -- Sets `g:_osv_nvim_output` as redir target for nvim messages, creates the
-          -- variable if it doesn't already exist, and clears it. Messages are still
-          -- displayed on-screen in debuggee as usual. This should also work when nvim
-          -- is headless.
-          pcall(vim.api.nvim_exec2, 'silent redir => g:_osv_nvim_output', nvim_exec2_opts)
-          stop_profiler("output")
-        end
-
         vim.wait(50)
       end
 
@@ -1506,31 +1431,6 @@ function M.attach()
 
         M.server_messages = {}
 
-        if redir_nvim_output and not vim.in_fast_event() then
-          start_profiler("output")
-
-          -- Sets `g:_osv_nvim_output` with messages observed since last `:redir`.
-          pcall(vim.api.nvim_exec2, 'silent redir END', nvim_exec2_opts)
-
-          local ok, msgs = pcall(vim.api.nvim_get_var, '_osv_nvim_output')
-          if ok then
-            if type(msgs) ~= 'string' then
-              error('expected g:_osv_nvim_output to be a string but got: ' .. msgs)
-            elseif #msgs > 0 then
-              local event = make_event 'output'
-              event.body = { category = 'stdout', output = msgs }
-              sendProxyDAP(event)
-            end
-          end
-
-          -- Sets `g:_osv_nvim_output` as redir target for nvim messages, creates the
-          -- variable if it doesn't already exist, and clears it. Messages are still
-          -- displayed on-screen in debuggee as usual. This should also work when nvim
-          -- is headless.
-          pcall(vim.api.nvim_exec2, 'silent redir => g:_osv_nvim_output', nvim_exec2_opts)
-          stop_profiler("output")
-        end
-
         vim.wait(50)
       end
 
@@ -1563,31 +1463,6 @@ function M.attach()
         end
 
         M.server_messages = {}
-
-        if redir_nvim_output and not vim.in_fast_event() then
-          start_profiler("output")
-
-          -- Sets `g:_osv_nvim_output` with messages observed since last `:redir`.
-          pcall(vim.api.nvim_exec2, 'silent redir END', nvim_exec2_opts)
-
-          local ok, msgs = pcall(vim.api.nvim_get_var, '_osv_nvim_output')
-          if ok then
-            if type(msgs) ~= 'string' then
-              error('expected g:_osv_nvim_output to be a string but got: ' .. msgs)
-            elseif #msgs > 0 then
-              local event = make_event 'output'
-              event.body = { category = 'stdout', output = msgs }
-              sendProxyDAP(event)
-            end
-          end
-
-          -- Sets `g:_osv_nvim_output` as redir target for nvim messages, creates the
-          -- variable if it doesn't already exist, and clears it. Messages are still
-          -- displayed on-screen in debuggee as usual. This should also work when nvim
-          -- is headless.
-          pcall(vim.api.nvim_exec2, 'silent redir => g:_osv_nvim_output', nvim_exec2_opts)
-          stop_profiler("output")
-        end
 
         vim.wait(50)
       end
