jbyuki/one-small-step-for-vimkind

commit 69c0dc61f2298862b3a38bdf3c4158c181cf15be

handle invalid level (#74)
handle invalid level error when inspect variables

---------

Co-authored-by: Julien Burkhard <jyburkhard@hotmail.com>

lua/osv/init.lua
@@ -844,8 +844,8 @@ function M.prepare_attach(blocking)
       local a = 1
       local frame = ref
       while true do
-        local ln, lv = debug.getlocal(frame, a)
-        if not ln then
+        local succ, ln, lv = pcall(debug.getlocal, frame, a)
+        if not succ or not ln then
           break
         end
 

src/handlers/variables.lua.t2
@@ -23,8 +23,8 @@ end
 local a = 1
 local frame = ref
 while true do
-  local ln, lv = debug.getlocal(frame, a)
-  if not ln then
+  local succ, ln, lv = pcall(debug.getlocal, frame, a)
+  if not succ or not ln then
     break
   end
 

commit 2d9bf5091e5f6a308e72c9a6c8e8e1e0e7ba571a

fix incorrect scope regression introduced in #74
`pcall` has its own stacklevels so it has be taken care of.

lua/osv/init.lua
@@ -500,8 +500,8 @@ function M.prepare_attach(blocking)
     local surface = 0
     local off = 0
     while true do
-      local info = debug.getinfo(off, "S")
-      if not info then
+      local succ, info = pcall(debug.getinfo, off, "S")
+      if not succ or not info then
         break
       end
 
@@ -700,8 +700,8 @@ function M.prepare_attach(blocking)
 
     local off = 0
     while true do
-      local info = debug.getinfo(off+levels+start_frame)
-      if not info then
+      local succ, info = pcall(debug.getinfo, off+levels+start_frame)
+      if not succ or not info then
         break
       end
 
@@ -724,11 +724,11 @@ function M.prepare_attach(blocking)
     end
 
 
-    -- @log_whole_stack_trace
+    -- ; log whole stack trace
 
     while levels <= max_levels or max_levels == -1 do
-      local info = debug.getinfo(skip+levels+start_frame)
-      if not info then
+      local succ, info = pcall(debug.getinfo,skip+levels+start_frame)
+      if not succ or not info then
         break
       end
 
@@ -791,8 +791,8 @@ function M.prepare_attach(blocking)
     local surface = 0
     local off = 0
     while true do
-      local info = debug.getinfo(off, "S")
-      if not info then
+      local succ, info = pcall(debug.getinfo, off, "S")
+      if not succ or not info then
         break
       end
 
@@ -839,6 +839,7 @@ function M.prepare_attach(blocking)
     local args = request.arguments
 
     local ref = vars_ref[args.variablesReference]
+    log("VAR REF " .. vim.inspect(ref))
     local variables = {}
     if type(ref) == "number" then
       local a = 1
@@ -868,29 +869,31 @@ function M.prepare_attach(blocking)
         a = a + 1
       end
 
-      local func = debug.getinfo(frame).func
-      local a = 1
-      while true do
-        local ln,lv = debug.getupvalue(func, a)
-        if not ln then break end
+      local succ, info = pcall(debug.getinfo, fname)
+      if succ and info and info.func then
+        local a = 1
+        while true do
+          local ln,lv = debug.getupvalue(info.func, a)
+          if not ln then break end
 
-        if vim.startswith(ln, "(") then
+          if vim.startswith(ln, "(") then
 
-        else
-          local v = {}
-          v.name = tostring(ln)
-          v.variablesReference = 0
-          if type(lv) == "table" then
-            vars_ref[vars_id] = lv
-            v.variablesReference = vars_id
-            vars_id = vars_id + 1
+          else
+            local v = {}
+            v.name = tostring(ln)
+            v.variablesReference = 0
+            if type(lv) == "table" then
+              vars_ref[vars_id] = lv
+              v.variablesReference = vars_id
+              vars_id = vars_id + 1
 
+            end
+            v.value = tostring(lv) 
+
+            table.insert(variables, v)
           end
-          v.value = tostring(lv) 
-
-          table.insert(variables, v)
+          a = a + 1
         end
-        a = a + 1
       end
     elseif type(ref) == "table" then
       for ln, lv in pairs(ref) do
@@ -972,8 +975,8 @@ function M.prepare_attach(blocking)
           local surface = 0
           local off = 0
           while true do
-            local info = debug.getinfo(off, "S")
-            if not info then
+            local succ, info = pcall(debug.getinfo, off, "S")
+            if not succ or not info then
               break
             end
 
@@ -1006,8 +1009,8 @@ function M.prepare_attach(blocking)
           local surface = 0
           local off = 0
           while true do
-            local info = debug.getinfo(off, "S")
-            if not info then
+            local succ, info = pcall(debug.getinfo, off, "S")
+            if not succ or not info then
               break
             end
 
@@ -1029,7 +1032,7 @@ function M.prepare_attach(blocking)
           end
 
 
-          local info = debug.getinfo(surface, "S")
+          local _, info = pcall(debug.getinfo, surface, "S")
           local source_path = info.source
 
           if source_path:sub(1, 1) == "@" then
@@ -1273,8 +1276,8 @@ function M.prepare_attach(blocking)
         	local surface = 0
         	local off = 0
         	while true do
-        	  local info = debug.getinfo(off, "S")
-        	  if not info then
+        	  local succ, info = pcall(debug.getinfo, off, "S")
+        	  if not succ or not info then
         	    break
         	  end
 
@@ -1296,9 +1299,9 @@ function M.prepare_attach(blocking)
         	end
 
 
-        	local info = debug.getinfo(surface)
+        	local succ, info = pcall(debug.getinfo, surface)
 
-        	if info and info.currentline and info.currentline ~= 0 then
+        	if succ and info and info.currentline and info.currentline ~= 0 then
         		valid = true
         	end
         	if valid then
@@ -1789,8 +1792,8 @@ function M.start_trace()
 		local surface = 0
 		local off = 0
 		while true do
-		  local info = debug.getinfo(off, "S")
-		  if not info then
+		  local succ, info = pcall(debug.getinfo, off, "S")
+		  if not succ or not info then
 		    break
 		  end
 
@@ -1812,7 +1815,7 @@ function M.start_trace()
 		end
 
 
-		local info = debug.getinfo(surface, "S")
+		local _, info = pcall(debug.getinfo, surface, "S")
 		local source_path = info.source
 		cache[source_path] = cache[source_path] or {}
 		table.insert(cache[source_path], line)

src/breakpoint/breakpoint_hit.lua.t2
@@ -9,7 +9,7 @@ if event == "line" and bps then
 local surface = 0
 ; get surface stack frame
 
-local info = debug.getinfo(surface, "S")
+local _, info = pcall(debug.getinfo, surface, "S")
 local source_path = info.source
 
 ;; script variables
@@ -104,8 +104,8 @@ path = os.getenv("VIMRUNTIME") .. "/lua/" .. source_path:sub(2)
 ;; get surface stack frame
 local off = 0
 while true do
-  local info = debug.getinfo(off, "S")
-  if not info then
+  local succ, info = pcall(debug.getinfo, off, "S")
+  if not succ or not info then
     break
   end
 

src/handlers/next.lua.t2
@@ -36,8 +36,8 @@ elseif event == "line" and next and depth >= 0 and depth <= stack_level then
 local surface = 0
 local off = 0
 while true do
-  local info = debug.getinfo(off, "S")
-  if not info then
+  local succ, info = pcall(debug.getinfo, off, "S")
+  if not succ or not info then
     break
   end
 

src/handlers/stack_trace.lua.t2
@@ -29,11 +29,11 @@ local skip = 0
 
 ; skip internal frames
 
--- @log_whole_stack_trace
+-- ; log whole stack trace
 
 while levels <= max_levels or max_levels == -1 do
-  local info = debug.getinfo(skip+levels+start_frame)
-  if not info then
+  local succ, info = pcall(debug.getinfo,skip+levels+start_frame)
+  if not succ or not info then
     break
   end
 
@@ -73,8 +73,8 @@ end
 ;; skip internal frames
 local off = 0
 while true do
-  local info = debug.getinfo(off+levels+start_frame)
-  if not info then
+  local succ, info = pcall(debug.getinfo, off+levels+start_frame)
+  if not succ or not info then
     break
   end
 
@@ -101,8 +101,8 @@ end
 ;; log whole stack trace
 local off = 0
 while true do
-  local info = debug.getinfo(off+levels+start_frame)
-  if not info then
+  local succ, info = pcall(debug.getinfo, off+levels+start_frame)
+  if not succ or not info then
     break
   end
   log("STACK " .. (info.name or "[NO NAME]") .. " " .. (info.source or "[NO SOURCE]") .. " " .. info.currentline)

src/handlers/step_in.lua.t2
@@ -45,8 +45,8 @@ sendProxyDAP(msg)
 local surface = 0
 ; get surface stack frame
 
-local info = debug.getinfo(surface)
+local succ, info = pcall(debug.getinfo, surface)
 
-if info and info.currentline and info.currentline ~= 0 then
+if succ and info and info.currentline and info.currentline ~= 0 then
 	valid = true
 end

src/handlers/variables.lua.t2
@@ -4,6 +4,7 @@ function handlers.variables(request)
   local args = request.arguments
 
   local ref = vars_ref[args.variablesReference]
+  log("VAR REF " .. vim.inspect(ref))
   local variables = {}
   if type(ref) == "number" then
     ; inspect local variables
@@ -61,17 +62,19 @@ for ln, lv in pairs(ref) do
 end
 
 ;; inspect up values
-local func = debug.getinfo(frame).func
-local a = 1
-while true do
-  local ln,lv = debug.getupvalue(func, a)
-  if not ln then break end
+local succ, info = pcall(debug.getinfo, fname)
+if succ and info and info.func then
+  local a = 1
+  while true do
+    local ln,lv = debug.getupvalue(info.func, a)
+    if not ln then break end
 
-  ; omit temporary values
-  else
-    local v = {}
-    ; fill variable struct
-    table.insert(variables, v)
+    ; omit temporary values
+    else
+      local v = {}
+      ; fill variable struct
+      table.insert(variables, v)
+    end
+    a = a + 1
   end
-  a = a + 1
 end

src/optimizations/optimize_stack_monitor.lua.t2
@@ -34,7 +34,7 @@ head_start_depth = off - 1
 
 ;; head start for monitoring
 if head_start_depth >= 0 then
-  local info = debug.getinfo(head_start_depth, "S")
+  local _, info = pcall(debug.getinfo, head_start_depth, "S")
   if info then
 		local inside_osv = false
 		; check if inside osv

src/trace/start_stop.lua.t2
@@ -17,7 +17,7 @@ function line_hook(event, line)
 	local surface = 0
 	; get surface stack frame
 
-	local info = debug.getinfo(surface, "S")
+	local _, info = pcall(debug.getinfo, surface, "S")
 	local source_path = info.source
 	; add to cache
 end